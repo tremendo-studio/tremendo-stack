03f13494cda72da743c52b9ec8d07b69259f6962
Hernan Poncetta <hernanponcetta@gmail.com>
2024-09-23 21:33:55 +0200

    remove docker compose

--- app/db/index.ts
+++ app/db/index.ts
@@ -4,6 +4,7 @@ import { drizzle } from "drizzle-orm/libsql"
 import env from "~/env"
 
 // For inspiration: https://github.com/w3cj/bytedash
+// https://andriisherman.medium.com/migrations-with-drizzle-just-got-better-push-to-sqlite-is-here-c6c045c5d0fb
 
 export const client = createClient({
   authToken: env.DB_TOKEN,
