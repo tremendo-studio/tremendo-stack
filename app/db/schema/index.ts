e9e3f5e9b55ae4016aaebea70a765138f6db9075
Hernan Poncetta <hernanponcetta@gmail.com>
2024-09-25 08:12:25 +0200

    add user session

--- app/db/schema/index.ts
+++ app/db/schema/index.ts
@@ -1,2 +1,3 @@
 export { default as authSession, insertAuthSessionSchema } from "./auth-session"
 export { default as user, insertUserSchema } from "./user"
+export { default as userSession, insertUserSessionSchema } from "./user-session"
